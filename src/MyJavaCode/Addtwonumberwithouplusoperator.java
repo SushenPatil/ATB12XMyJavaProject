39739e1dfe19b11d786cf0428c94bf81224f3c6f
SushenPatil <sushenp85@gmail.com>
2025-05-13 16:26:01 +0530

    Comment for literal

---
+++ src/MyJavaCode/Addtwonumberwithouplusoperator.java
@@ -0,0 +1,11 @@
+package MyJavaCode;
+
+public class Addtwonumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 50;
+        int d = a-(-b);
+        System.out.println(d-(-c));
+    }
+}
