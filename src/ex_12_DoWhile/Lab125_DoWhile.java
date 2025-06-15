2d4922157f8bf434ba11af32ee67629b06b13845
SushenPatil <sushenp85@gmail.com>
2025-06-15 13:13:59 +0530

    25th May 2025 - p1

---
+++ src/ex_12_DoWhile/Lab125_DoWhile.java
@@ -0,0 +1,13 @@
+package ex_12_DoWhile;
+
+public class Lab125_DoWhile {
+    public static void main(String[] args){
+
+        int i =1;
+        do{
+            System.out.println(" body!");
+            System.out.println(i);
+            i++;
+        }while(i<10);
+    }
+}
