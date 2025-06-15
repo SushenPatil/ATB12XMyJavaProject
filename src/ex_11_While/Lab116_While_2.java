e16d8bfd2e56c6640c15711cdfd2c68582c160ec
SushenPatil <sushenp85@gmail.com>
2025-06-15 13:13:38 +0530

    25th May 2025 - p1

---
+++ src/ex_11_While/Lab116_While_2.java
@@ -0,0 +1,14 @@
+package ex_11_While;
+
+public class Lab116_While_2 {
+    public static void main(String[] args) {
+
+        int modi= 10;
+        while(modi<=15){
+            System.out.println(" Modi will become PM of india for 15 years");
+            modi++;
+
+
+        }
+    }
+}
