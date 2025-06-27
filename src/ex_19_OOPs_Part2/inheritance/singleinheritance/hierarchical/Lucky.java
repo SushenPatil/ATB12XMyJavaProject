1299168f6ed302ded88a88be248e4e518ceb5602
SushenPatil <sushenp85@gmail.com>
2025-06-27 13:47:20 +0530

    7th June 2025

---
+++ src/ex_19_OOPs_Part2/inheritance/singleinheritance/hierarchical/Lucky.java
@@ -0,0 +1,9 @@
+package ex_19_OOPs_Part2.inheritance.singleinheritance.hierarchical;
+
+import ex_19_OOPs_Part2.inheritance.singleinheritance.multilevel.Father;
+
+public class Lucky extends Father {
+    void l2(){
+        System.out.println("Lucky");
+    }
+}
---
+++ src/ex_19_OOPs_Part2/inheritance/singleinheritance/hierarchical/Pramod.java
@@ -0,0 +1,9 @@
+package ex_19_OOPs_Part2.inheritance.singleinheritance.hierarchical;
+
+import ex_19_OOPs_Part2.inheritance.singleinheritance.multilevel.Father;
+
+public class Pramod extends Father {
+    void h2(){
+        System.out.println("h2 - Pramod");
+    }
+}
